6d5a16183c6054fe4eec78740b4f7ddf0860b3f8
z1912129 <70195194+z1912129@users.noreply.github.com>
2022-01-25 05:05:35 -0600

    Add files via upload

---
+++ assignment00.cc
@@ -0,0 +1,18 @@
+/* 	Eric M Baumbach
+	Z1912129
+	CSCI 480-PE1
+	
+	I certify that this is my own work and where appropriate an extension 
+	of the starter code provided for the assignment.
+*/
+
+#include <iostream>
+
+int main()
+{
+
+	std::cut << "I hope I am submitting this asssignment correctly." << std::endl;
+	
+	return 0;
+
+}
