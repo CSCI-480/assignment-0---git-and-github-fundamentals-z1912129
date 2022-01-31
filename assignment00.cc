18743756151871fd58ce34573383973ca8c5eb38
z1912129 <70195194+z1912129@users.noreply.github.com>
2022-01-31 09:40:46 -0600

    re-add print line

--- assignment00.cc
+++ assignment00.cc
@@ -5,3 +5,14 @@
 	I certify that this is my own work and where appropriate an extension 
 	of the starter code provided for the assignment.
 */
+
+#include <iostream>
+
+int main()
+{
+
+	std::cut << "I hope I am submitting this asssignment correctly." << std::endl;
+	
+	return 0;
+
+}
